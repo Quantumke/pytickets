f315449a40512067d69f871bce97181b55177426
quantumke <nguruben@gmail.com>
2016-09-03 21:27:06 +0300

    pesapal handler model

--- app/models.py
+++ app/models.py
@@ -67,6 +67,48 @@ class newsletter(models.Model):
 	def __unicode__(self):
 		return self.email
 
+class Pesapal(models.Model):
+    PESAPAL_STATUS_CHOICES = (
+                ('PENDING', 'Pending'),
+                ('COMPLETED', 'Completed'),
+                ('FAILED', 'Failed'),
+                ('INVALID', 'Invalid'),
+            )
+    tracking_id = models.CharField(max_length=50, verbose_name='Pesapal Transaction Tracking ID')
+    reference = models.CharField(max_length=50, verbose_name='Pesapal Merchant Reference')
+    status = models.CharField(max_length=9, choices=PESAPAL_STATUS_CHOICES, default='PENDING')
+
+    class Meta:
+        verbose_name = 'Pesapal Payment'
+        verbose_name_plural = 'Pesapal Payments'
+
+        def __unicode__(self):
+            return 'Payment %s for '%(self.status,self.reference)
+
+
+class Pesapal(models.Model):
+    """
+    Model to hold pesapal callback info
+    """
+    #<PENDING|COMPLETED|FAILED|INVALID>
+    PESAPAL_STATUS_CHOICES = (
+                ('PENDING', 'Pending'),
+                ('COMPLETED', 'Completed'),
+                ('FAILED', 'Failed'),
+                ('INVALID', 'Invalid'),
+            )
+    tracking_id = models.CharField(max_length=50, verbose_name='Pesapal Transaction Tracking ID')
+    reference = models.CharField(max_length=50, verbose_name='Pesapal Merchant Reference')
+    status = models.CharField(max_length=9, choices=PESAPAL_STATUS_CHOICES, default='PENDING')
+
+    class Meta:
+        verbose_name = 'Pesapal Payment'
+        verbose_name_plural = 'Pesapal Payments'
+
+        def __unicode__(self):
+            return 'Payment %s for '%(self.status,self.reference)
+
+
 
 
 
