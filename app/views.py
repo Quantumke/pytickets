7d75a3ac06fb9f4588010e5547f81d740a9c944f
quantumke <nguruben@gmail.com>
2016-09-03 21:29:56 +0300

    slug method

--- app/views.py
+++ app/views.py
@@ -109,4 +109,58 @@ def homepage(request):
 
 		}, context_instance=context)
 
+def view_more(request, slug):
+	header_events1=Event.objects.latest('id')
+	f1l1= header_events1.title
+	id=header_events1.pk
+	f1l1= list(f1l1)
+	f1l1= f1l1[0]
 
+	header_events2=Event.objects.get(id=id-1)
+	f2l1= header_events2.title
+	f2l1= list(f2l1)
+	f2l1= f2l1[0]
+
+	header_events3=Event.objects.get(id=id-2)
+	f3l1= header_events3.title
+	f3l1= list(f3l1)
+	f3l1= f3l1[0]
+	header_events4=Event.objects.get(id=id-3)
+	f4l1= header_events4.title
+	f4l1= list(f4l1)
+	f4l1= f4l1[0]
+	current_obj=Event.objects.get(slug=slug)
+	title=current_obj.title
+	title=list(title)
+	title= title[0]
+	suggestion=Event.objects.all()[:4]
+	now = datetime.now().date()
+	event_date=current_obj.event_date
+	hours= now-event_date
+	a= hours.total_seconds()
+	b= a/3600
+	days= b/24
+	days=int(days)
+	days=days * -1
+	sec= a * -1
+
+
+
+
+	return render_to_response('single-event.html',{
+	'object':get_object_or_404(Event, slug=slug),
+
+		'header_events1':header_events1,
+		'f1l1':f1l1,
+		'header_events2':header_events2,
+		'f2l1':f2l1,
+		'header_events3':header_events3,
+		'f3l1':f3l1,
+		'header_events4':header_events4,
+		'f4l1':f4l1,
+		'title':title,
+		'suggestion':suggestion,
+		'days':days,
+		'sec':sec,
+
+	},RequestContext
